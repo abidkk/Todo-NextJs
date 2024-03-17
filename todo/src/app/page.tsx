d81d1e5764526c010c38b8914dc872ab82376355
abidkk <dev.abidkk@gmail.com>
2024-03-17 07:10:52 +0500

    form added

---
+++ todo/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from "@/components/Form"
+const page = () => {
+  return (
+    <>
+    <h1 className='text-center text-4xl font-bold text-orange-500 p-4'>Home Page</h1>
+
+    <Form/>
+
+    </>
+  )
+}
+
+export default page
